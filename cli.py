b730dfecea4b67067647d3cf898c0c99d7740fd3
JamesMcMahon <git.spamfriedrice@spamgourmet.com>
2012-04-01 15:43:17 -0400

    basic use of argparse in the cli

--- cli.py
+++ cli.py
@@ -1,7 +1,9 @@
+import argparse
 import os
 import sys
 
 ds = None
+parser = argparse.ArgumentParser(prog='Nom', description='Simple storage app')
 
 def cli_datastruct(dict):
 	global ds
@@ -13,21 +15,24 @@ def cli_datastruct(dict):
 			dict[name] = fn
 			return fn
 		return cli_decorator
-	return cli_definer 
+	return cli_definer
+
+# FIXME this might be an abuse the type check for argparse
+def file_check(filename):
+	if not os.path.exists(filename):
+		msg = 'invalid filename ' + filename
+		raise argparse.ArgumentTypeError(msg)
+	if os.path.isdir(filename):
+		msg = "can't operate on directories " + filename
+		raise argparse.ArgumentTypeError(msg)
+	return filename
 
 def dispatch(cfg):
-	args = sys.argv[1:] 
+	parser.add_argument('func')
+	parser.add_argument('filenames', type=file_check, nargs="+")
+	args = parser.parse_args()
 
-	# Temp code, gets first args, dispatches
-	func = ds[args[0]]
-	
-	args = args[1:]
-	for filename in args:
-			if not os.path.exists(filename):
-				print 'invalid filename ' + filename
-				continue
-			if os.path.isdir(filename):
-				print "can't operate on directories " + filename
-				continue
-			func(filename, cfg)
+	func = ds[args.func]
+	for filename in args.filenames:
+		func(filename, cfg)
 
