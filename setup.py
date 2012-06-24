0b8c9326021a37341149f053aa97cc0e7f376f79
JamesMcMahon <git.spamfriedrice@spamgourmet.com>
2012-06-23 21:25:22 -0400

    Added more info to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from distutils.core import setup
 setup(
 	name='nom',
 	version='0.1',
+	url="https://github.com/JamesMcMahon/nom",
+	license="MIT License",
+	author="James McMahon",
+	description="Simple app to store configuration files",
 	packages=['nom'],
 	entry_points={
 		'console_scripts': [
